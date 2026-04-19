7c1f3afe45f90b205cf5c157422122e61aee2dc0
irenacarkovova-design <irena.carkovova@gmail.com>
2026-04-19 18:44:36 +0200

    Refactor service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
 const cacheName = 'menovnik-v1';
-const assets = ['index.html', 'manifest.json', 'style.css', 'app.js'];
-self.addEventListener('install', event => {
-    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));{
-                return cache.addAll(assets);
-            }
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+// Instalace - uloží soubory do cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+// Aktivace - vyčistí starou cache
+self.addEventListener('activate', e => {
+  console.log('Service Worker aktivován');
+});
+
+// Fetch - obsluha požadavků (tady se děje to offline kouzlo)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
